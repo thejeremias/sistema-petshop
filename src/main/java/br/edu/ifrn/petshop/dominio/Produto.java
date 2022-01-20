2f37d0692224d8ed01911dd25c0845b7a86467a1
Jeremias Saldanha <saldanhajeremias295@gmail.com>
2022-01-19 21:19:49 -0300

    Remocao de boilerplate code e inclusao de Lombok

--- src/main/java/br/edu/ifrn/petshop/dominio/Produto.java
+++ src/main/java/br/edu/ifrn/petshop/dominio/Produto.java
@@ -3,6 +3,9 @@ package br.edu.ifrn.petshop.dominio;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import lombok.Data;
+
+@Data
 public class Produto {
 
 	private int id;
@@ -18,58 +21,4 @@ public class Produto {
 	/*A validação deste campo foi feita toda pelo HTML mesmo*/
 	private int quantidade;
 	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-
-	public int getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Produto other = (Produto) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
-
 }
